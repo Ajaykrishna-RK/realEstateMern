82c7756d175f41d4c03cc20c81c9661427595448
Ajay <ajayajuu73@gmail.com>
2023-11-05 08:02:19 +0530

    data base added

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
-import express from  "express"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express()
+dotenv.config();
 
-app.listen(5000,()=>{
-    console.log('Server is running at port 3000 ')
-})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then((res) => {
+    console.log(`Connected to Database `);
+  })
+  .catch((err) => {
+    console.log(err, "Database not connected");
+  });
+
+const app = express();
+
+app.listen(5000, () => {
+  console.log("Server is running at port 3000");
+});
