84d3af3c1a668778676120b057743f5c3f5a5d9e
Ajay <ajayajuu73@gmail.com>
2023-11-21 21:25:22 +0530

    signUp Page Ui completed

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import {Link} from "react-router-dom"
 
 function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="my-7 text-3xl text-center font-semibold"> Sign Up </h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg focus:outline-none"
+          id="username"
+        />
+         <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg focus:outline-none"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg focus:outline-none"
+          id="password"
+        />
+
+        <button  className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-70"> Sign Up </button>
+      </form>
+
+      <div className="flex items-center gap-2 mt-5">
+        <p > Have an account ? </p>
+        <Link to={"/sign-in"}> <span className="text-blue-700"> Sign In </span> </Link>
+        
+      </div>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,15 +1,15 @@
-import User from "../models/User.model.js"
-import bcryptjs from "bcryptjs"
+import User from "../models/User.model.js";
+import bcryptjs from "bcryptjs";
 
-export const signUp = async (req,res)=>{
-     try{
-const {userName, email,password} = req.body
-const hashPassword = bcryptjs.hashSync(password,10) 
-const newUser = new User({userName,email,password:hashPassword}) 
-await newUser.save()
+export const signUp = async (req, res, next) => {
+  try {
+    const { userName, email, password } = req.body;
+    const hashPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ userName, email, password: hashPassword });
+    await newUser.save();
 
-res.status(201).json({newUser})
-     }catch(err){
-        res.status(500).json({err:err})
-     }
-}
+    res.status(201).json({ newUser });
+  } catch (err) {
+    next(err);
+  }
+};
