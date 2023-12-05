a5adbdd951870d1bb9b03b0581b5e9615e22fc81
Ajay <ajayajuu73@gmail.com>
2023-12-05 21:29:15 +0530

    private route added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <header className="bg-slate-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -21,18 +24,24 @@ function Header() {
           <FaSearch className="text-slate-600" />
         </form>
         <ul className="flex gap-4">
-            <Link to={"/"}>
-          <li className="hidden sm:inline text-slate-700 hover:underline">
-            Home
-          </li>
+          <Link to={"/"}>
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
           </Link>
           <Link to={"/about"}>
-          <li className="hidden sm:inline text-slate-700 hover:underline">
-            About
-          </li>
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
           </Link>
-          <Link to={"/sign-in"}>
-          <li className=" text-slate-700 hover:underline">Sign in</li>
+          <Link to={"/profile"}>
+            {currentUser ? (
+              <>
+                <img className="w-7 h-7 object-cover rounded-full" src={currentUser?.avatar} alt="profile" />
+              </>
+            ) : (
+              <li className=" text-slate-700 hover:underline">Sign in</li>
+            )}
           </Link>
         </ul>
       </div>
